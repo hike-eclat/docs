38ba3aee98b6013bdd8073b85cebb2425d1a6862
hike-eclat <86474225+hike-eclat@users.noreply.github.com>
2021-12-01 01:00:14 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,13 +28,17 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.extlinks',
+              'sphinxcontrib.spelling',
+              'myst_parser',
+             ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
